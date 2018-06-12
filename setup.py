fa878e1cfe04ad9012208a4204fc300575aac74b
Romain Brault <romain.brault@protonmail.com>
2018-06-12 11:32:18 +0200

    manifest

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""Setup script for tekel."""
+from __future__ import print_function
+
+import sys
+
+from re import search
+from setuptools import setup, find_packages
+from tekel import __authors__, __version__
+
+
+def main():
+    with open('requirements.txt') as f:
+        INSTALL_REQUIRES = [l.strip()
+                            for l in f.readlines()
+                            if l and not search('# optional', l)]
+        print(INSTALL_REQUIRES)
+
+    setup(name='tekel',
+          version=__version__,
+          description='A TensorFlow kernel library',
+          long_description=open('README.rst').read(),
+          license='MIT',
+          authors=__authors__,
+          packages=find_packages(),
+          install_requires=INSTALL_REQUIRES,
+          author_email='mail@romainbrault.com',
+          )
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
---
+++ tekel/__init__.py
@@ -0,0 +1,6 @@
+"""TensorFlow Kernel Library: tekel."""
+
+__version__ = '0.0.1-rc0'
+__authors__ = ['Romain Brault']
+
+__all__ = []
